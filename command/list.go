e80527e12b588df86d3d0fa88069eba3f68d8ff5
r_takaishi <ryo.takaishi.0@gmail.com>
2018-10-10 09:13:32 +0900

    list: read template files directly

--- command/list.go
+++ command/list.go
@@ -1,21 +1,50 @@
 package command
 
 import (
+	"github.com/gernest/front"
 	"github.com/olekukonko/tablewriter"
-	patt "github.com/takaishi/patt/lib"
 	"github.com/urfave/cli"
+	"io/ioutil"
 	"os"
 )
 
+type Template struct {
+	Name        string
+	Source      string
+	Destination string
+}
+
+type Templates []Template
+
 func RunListCommand(c *cli.Context) error {
-	configs := patt.ReadConfig()
+	templates := Templates{}
+	files, err := ioutil.ReadDir(os.Getenv("HOME") + "/.patt.d/templates")
+	if err != nil {
+		return err
+	}
+
+	for _, file := range files {
+		src := os.Getenv("HOME") + "/.patt.d/templates/" + file.Name()
+		doc := readTemplateFile(src, getVariables())
+
+		m := front.NewMatter()
+		m.Handle("+++", front.JSONHandler)
+		f, _, err := m.Parse(&doc)
+		if err != nil {
+			return err
+		}
+		name := f["name"].(string)
+		dst := f["destination"].(string)
+		templates = append(templates, Template{Name: name, Source: src, Destination: dst})
+	}
 
 	table := tablewriter.NewWriter(os.Stdout)
 	table.SetHeader([]string{"Name", "Source"})
-	for k := range configs {
-		table.Append([]string{k, configs[k].Source})
+	for _, t := range templates {
+		table.Append([]string{t.Name, t.Source})
 	}
 
 	table.Render()
+
 	return nil
 }
