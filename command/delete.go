244897826ad47675549a0b64d715f2f109b69e12
r_takaishi <ryo.takaishi.0@gmail.com>
2018-10-10 09:25:00 +0900

    delete: read template files directly

--- command/delete.go
+++ command/delete.go
@@ -1,27 +1,46 @@
 package command
 
 import (
-	patt "github.com/takaishi/patt/lib"
+	"errors"
+	"github.com/gernest/front"
 	"github.com/urfave/cli"
+	"io/ioutil"
 	"os"
 )
 
 func RunDeleteCommand(c *cli.Context) error {
-	key := c.Args().Get(0)
-	configs := patt.ReadConfig()
+	templates := Templates{}
+	name := c.Args().Get(0)
 
-	path := configs[key].Source
-	err := os.Remove(path)
+	files, err := ioutil.ReadDir(os.Getenv("HOME") + "/.patt.d/templates")
 	if err != nil {
 		return err
 	}
 
-	delete(configs, key)
+	for _, file := range files {
+		src := os.Getenv("HOME") + "/.patt.d/templates/" + file.Name()
+		doc := readTemplateFile(src, getVariables())
 
-	err = patt.WriteConfig(configs)
-	if err != nil {
-		return err
+		m := front.NewMatter()
+		m.Handle("+++", front.JSONHandler)
+		f, _, err := m.Parse(&doc)
+		if err != nil {
+			return err
+		}
+		name := f["name"].(string)
+		dst := f["destination"].(string)
+		templates = append(templates, Template{Name: name, Source: src, Destination: dst})
 	}
 
-	return nil
+	for _, template := range templates {
+		if template.Name == name {
+			err := os.Remove(template.Source)
+			if err != nil {
+				return err
+			}
+			return nil
+		}
+	}
+
+	return errors.New("Cound not find template " + name)
 }
